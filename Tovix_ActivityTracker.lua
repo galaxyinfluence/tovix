2066b4654f387abc7d38c86fa58707e2f0234d0b
GalaxyInfluence <139978578+galaxyinfluence@users.noreply.github.com>
2025-04-29 10:11:40 +0100

    Update Tovix_ActivityTracker.lua

--- Tovix_ActivityTracker.lua
+++ Tovix_ActivityTracker.lua
@@ -1,3 +1,16 @@
 local players = game:GetService("Players")
-local api_key = "API_KEY_HERE"
 local httpservice = game:GetService("HttpService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local RunService = game:GetService("RunService")
+
+if RunService:IsStudio() or game.PrivateServerId ~= "" and game.PrivateServerOwnerId ~= 0 then
+  return warn("Tovix Activity : Your activity does not track in private servers nor studio instances.")
+end
+
+local configuration = {
+  -- These are already set up, do not touch them
+  ["API_KEY"] = "API_KEY_HERE",
+  -- Configure the stuff below to your liking
+  ["RankChecking"] = true,
+  ["GroupId"] = 0,
+}
